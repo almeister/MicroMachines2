b16ed1689db8bdc68354d8c80f7adb9ed1595af2
Alex Coaton <alex.coaton@gmail.com>
2016-11-15 23:03:20 +0000

    Added engine idle audio to Vehicle.

---
+++ Assets/Scripts/Vehicle.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Vehicle : MonoBehaviour {
+
+    private AudioSource audioSource;
+    private AudioClip engineIdleAudioClip;
+
+	// Use this for initialization
+	void Start () {
+        audioSource = GetComponent<AudioSource>();
+        engineIdleAudioClip = Resources.Load<AudioClip>("Audio/SFX/Engine Idle");
+        audioSource.clip = engineIdleAudioClip;
+        audioSource.loop = true;
+        audioSource.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
