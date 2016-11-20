4867cc1e5fccd3354b04c65c08cddceff877275b
Alex Coaton <alex.coaton@gmail.com>
2016-11-20 22:43:59 +0000

    Added acceleration and reverse audio effects to Vehicle.

--- Assets/Scripts/Vehicle.cs
+++ Assets/Scripts/Vehicle.cs
@@ -5,18 +5,47 @@ public class Vehicle : MonoBehaviour {
 
     private AudioSource audioSource;
     private AudioClip engineIdleAudioClip;
+    private float audioSourceStartingPitch;
+    private float audioSourceMaxPitch;
+    private float audioSourceRampUpTime = 2.0f;
+    private float audioSourceMaxPitchMultiplier = 5.0f;
+    private float audioSourceRampDownTime = 0.5f;
+    private float reverseAudioMaxPitchMultiplier = 3.0f;
+    private float reverseAudioMaxPitch;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         audioSource = GetComponent<AudioSource>();
         engineIdleAudioClip = Resources.Load<AudioClip>("Audio/SFX/Engine Idle");
         audioSource.clip = engineIdleAudioClip;
         audioSource.loop = true;
         audioSource.Play();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+        audioSourceStartingPitch = audioSource.pitch;
+        audioSourceMaxPitch = audioSourceMaxPitchMultiplier * audioSourceStartingPitch;
+        reverseAudioMaxPitch = reverseAudioMaxPitchMultiplier * audioSourceStartingPitch;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetButton("A"))
+        {
+            if (audioSource.pitch <= audioSourceMaxPitch)
+                audioSource.pitch += Time.deltaTime * audioSource.pitch / (audioSourceRampUpTime / audioSourceMaxPitchMultiplier);
+
+            return;
+        }
+
+        if (Input.GetButton("B"))
+        {
+            if (audioSource.pitch <= reverseAudioMaxPitch)
+                audioSource.pitch += Time.deltaTime * audioSource.pitch / (audioSourceRampUpTime / reverseAudioMaxPitchMultiplier);
+
+            return;
+        }
+
+        {
+            if (audioSource.pitch > audioSourceStartingPitch)
+                audioSource.pitch -= Time.deltaTime * audioSource.pitch / audioSourceRampDownTime;
+        }
+    }
 }
