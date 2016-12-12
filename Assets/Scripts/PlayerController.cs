627bad90c2b9b950f1035381186c244adf158cfc
Alex Coaton <alex.coaton@gmail.com>
2016-12-12 21:35:48 +0000

    Braking with come to rest.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,12 +3,17 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-    private float drivePower = 10;
-    private float maxSpeed = 20;
-    private float turningPower = 5;
-    private float friction = 3;
-    private Vector2 currentSpeed;
+    private const float drivePower = 10;
+    private const float brakePower = 2;
+    private const float reversePower = 5;
+    private const float turningPower = 5;
+    private const float brakeRestTime = 0.3f;
+
     private Rigidbody2D rigidBody;
+    private float friction = 3;
+    private float previousSpeed = 0.0f;
+    private float timeAtRest = 0.0f;
+    private bool isResting = false;
 
     void Start()
     {
@@ -18,28 +23,46 @@ public class PlayerController : MonoBehaviour {
 
     void FixedUpdate()
     {
-        currentSpeed = new Vector2(rigidBody.velocity.x, rigidBody.velocity.y);
-
-        if (currentSpeed.magnitude > maxSpeed)
-        {
-            currentSpeed = currentSpeed.normalized;
-            currentSpeed *= maxSpeed;
-        }
-
         if (Input.GetButton("A"))
         {
             rigidBody.AddForce(transform.up * drivePower);
             rigidBody.drag = friction;
         }
+
         if (Input.GetButton("B"))
         {
-            rigidBody.AddForce(-(transform.up) * (drivePower / 2));
+            float currentSpeed = transform.InverseTransformDirection(rigidBody.velocity).y;
+
+            if (previousSpeed > 0 && currentSpeed <= 0 || isResting)
+            {
+                timeAtRest += Time.deltaTime;
+                isResting = true;
+
+                if (timeAtRest >= brakeRestTime)
+                {
+                    timeAtRest = 0.0f;
+                    isResting = false;
+                }
+            }
+
+            if (isResting)
+            {
+                // Resting... wait
+            }
+            else if (currentSpeed > 0)
+                rigidBody.AddForce(-(transform.up) * (brakePower));
+            else
+                rigidBody.AddForce(-(transform.up) * (reversePower));
+
             rigidBody.drag = friction;
+            previousSpeed = currentSpeed;
         }
+
         if (Input.GetAxis("Horizontal") < 0)
         {
             transform.Rotate(Vector3.forward * turningPower);
         }
+
         if (Input.GetAxis("Horizontal") > 0)
         {
             transform.Rotate(Vector3.forward * -turningPower);
@@ -50,19 +73,15 @@ public class PlayerController : MonoBehaviour {
 
     void noGas()
     {
-        bool gas;
+        bool gas = false;
         if (Input.GetButton("A") || Input.GetButton("B"))
         {
             gas = true;
         }
-        else
-        {
-            gas = false;
-        }
 
         if (!gas)
         {
-            rigidBody.drag = friction * 2;
+            rigidBody.drag = friction * 1.5f;
         }
     }
 }
